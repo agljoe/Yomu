f1c3a898d1c2e59ba72a6793c054267b66cb1622
Andrew <52801745+AndrewJ04@users.noreply.github.com>
2024-06-04 17:41:11 -0700

    Initial Commit

---
+++ Yomu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yomu
+//
+//  Created by Andrew Joe on 2024-06-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yomu/YomuApp.swift
@@ -0,0 +1,17 @@
+//
+//  YomuApp.swift
+//  Yomu
+//
+//  Created by Andrew Joe on 2024-06-04.
+//
+
+import SwiftUI
+
+@main
+struct YomuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
